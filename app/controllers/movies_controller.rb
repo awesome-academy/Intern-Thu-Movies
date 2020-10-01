f772048b24475295006e4fc14c60714f6a7a8cef
Thu99 <nguyenvanthu591999@gmail.com>
2020-10-01 16:00:35 +0700

    detail

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.page params[:page]
   end
+
+  def show
+    @movie = Movie.includes(:casts).find_by slug: params[:slug]
+    @recommend = Genre.includes(:movies).find_by id: @movie.genre_id
+  end
 end
